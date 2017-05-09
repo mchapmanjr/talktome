313b8e99244cc8a2f32286ebfc77ace86af2fa91
jimmy <jimmy_chau@yahoo.com>
2017-05-09 16:04:48 -0700

    New service code, jdk 1.8 pom

--- src/main/java/com/capgroup/googhome/controller/PortfolioController.java
+++ src/main/java/com/capgroup/googhome/controller/PortfolioController.java
@@ -1,5 +1,7 @@
 package com.capgroup.googhome.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,16 +11,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.capgroup.googhome.JSONResponse;
 import com.capgroup.googhome.domain.PortfolioRecommendation;
+import com.frogermcs.gactions.ResponseBuilder;
+import com.frogermcs.gactions.api.request.Inputs;
+import com.frogermcs.gactions.api.request.RootRequest;
+import com.frogermcs.gactions.api.response.RootResponse;
 
 @Controller
 @RequestMapping(value = "/portfolio")
 public class PortfolioController {
 
 	
+	
 	@RequestMapping(value = "/recommend", method = RequestMethod.POST)
-	public @ResponseBody JSONResponse getPortfolioRecommendation(@RequestBody PortfolioRecommendation portfolioR) {
-		return new JSONResponse("OK", portfolioR.getAge());
+	public @ResponseBody RootResponse getPortfolioRecommendation(@RequestBody RootRequest rootRequest) {
+		
+		
+		
+		List<Inputs> inputs = rootRequest.inputs;
+		
+		
+		RootResponse response = ResponseBuilder.tellResponse("I received your question " + rootRequest.user.profile.display_name);
+		
+	
+		return response;
 		
 	}
-
+	
 }
